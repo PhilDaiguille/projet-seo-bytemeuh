056e401d3432fb99b4619e76e64033c5d151226d
PhilDaiguille <philippe.delente@gmail.com>
2026-03-13 11:00:37 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { glob } from 'glob';
 import compression from 'vite-plugin-compression2';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import sitemap from 'vite-plugin-sitemap';
 const htmlFiles = glob.sync('**/*.html', {
-  ignore: ['node_modules/**', 'dist/**']
+  ignore: ['node_modules/**', 'dist/**', '.nuxt/**', '.output/**']
 });
 
 const input = {};
@@ -11,6 +14,14 @@ htmlFiles.forEach(file => {
   const name = file.replace(/\.html$/, '').replace(/\//g, '_');
   input[name] = resolve(__dirname, file);
 });
+
+const sitemapRoutes = htmlFiles
+  .filter(file => file !== '404.html') // Exclure la page 404
+  .map(file => {
+    let route = file.replace(/\/index\.html$/, '/').replace(/^index\.html$/, '');
+    if (!route.startsWith('/')) route = '/' + route;
+    return route;
+  });
 
 export default defineConfig({
   root: '.',
@@ -24,6 +35,9 @@ export default defineConfig({
         entryFileNames: 'assets/js/[name]-[hash].js',
         chunkFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: (assetInfo) => {
+          if (!assetInfo.name) {
+            return 'assets/[name]-[hash][extname]';
+          }
           const info = assetInfo.name.split('.');
           const ext = info[info.length - 1];
           if (/\.(css|scss|sass)$/i.test(assetInfo.name)) {
@@ -68,12 +82,51 @@ export default defineConfig({
     port: 4173
   },
   plugins: [
+    // Compression Brotli pour de meilleures performances
     compression({
       algorithms: [
         ["brotliCompress", { level: 11 }],
       ],
-
       threshold: 1024,
       deleteOriginalAssets: false,
-    })]
+    }),
+    
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+    
+    ViteImageOptimizer({
+      png: {
+        quality: 85,
+      },
+      jpeg: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
+      webp: {
+        quality: 85,
+      },
+    }),
+    
+    sitemap({
+      hostname: 'https://bytemeuh.phildaiguille.fr',
+      dynamicRoutes: sitemapRoutes,
+      exclude: ['/404', '/404.html'],
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date(),
+      readable: true,
+    }),
+  ]
 });
